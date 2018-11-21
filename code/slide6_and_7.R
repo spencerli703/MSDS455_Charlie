5aa404f47067559b9798009d8a28143dde6f2479
Anna Sheets <asheets@ASheets.local>
2018-11-21 16:10:44 -0700

    latest version

--- code/slide6_and_7.R
+++ code/slide6_and_7.R
@@ -4,13 +4,10 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
 
-# set working directory
-setwd("~/Documents/GradSchool/MSDS455/git_repos/MSDS455_Charlie/code")
-
 ## theme
 
 # https://material.io/design/color/the-color-system.html#tools-for-picking-colors
-pal_heart <- c("#FF8A80", "#D50000", "#FF1744",  "#FF5252", "#B71C1C")
+pal_heart <- c("#FF8A80", "#D50000", "#FF1744",  "#FF5252", "#B71C1C","#7f0000")
 blue_heart <- "#3F69AA"
 
 ############# color pieces!
@@ -63,8 +60,42 @@ theme_heart <- function(base_size = 20, base_family = "Helvetica",
     )
 }
 
-p <- ggplot(iris, aes(Sepal.Length, Sepal.Width, colour = Species))+
-  geom_point()
-# Use economist color scales
-p + scale_color_discrete_heart() + theme_heart() + ggtitle("Sample Heart Theme")
+# set working directory
+setwd("~/Documents/GradSchool/MSDS455/git_repos/MSDS455_Charlie/data")
 
+data <- read.table(file="slide6_lifestyle-habits-of-us-adults-diagnosed-with-cardiovascular-conditions-2018-by-age.csv",sep=",",stringsAsFactors=FALSE,header=TRUE,quote="'")
+
+plot_data <- reshape(data,
+                         varying = c("total", "age_18_34" , "age_35_44" , "age_45_54" , "age_55_64" , "age_65_plus"),
+                         v.names = "Percent",
+                         idvar = "factor",
+                         times = c("total", "age_18_34" , "age_35_44" , "age_45_54" , "age_55_64" , "age_65_plus"),
+                         direction="long")
+
+rownames(plot_data) <- NULL
+
+plot_data$time <- factor(plot_data$time , labels=c('18-34 year olds','35-44 year olds','45-54 year olds','55-64 year olds','65+ year olds','Overall'))
+
+##Bar graph to illustrate mean rating by grad year/program
+
+#Function to make the title wrap since it's so long
+wrapper <- function(x, ...) 
+{
+  paste(strwrap(x, ...), collapse = "\n")
+}
+
+setwd("~/Documents/GradSchool/MSDS455/git_repos/MSDS455_Charlie/visualizations")
+pdf(file = "slide_6_causes_of_HD.pdf", 
+    width = 8, height = 10, paper = "letter")  
+ggplot_object <- ggplot(data=plot_data,
+                        aes(x=factor, y=Percent)) +
+  geom_bar(aes(fill = time), position = "dodge", stat="identity") +
+  theme_heart() +
+  scale_fill_heart() +
+  ggtitle(wrapper("Percentage of U.S. adults with cardiovascular conditions that had select lifestyle habits as of 2018, by age",width=55)) +
+  xlab("Lifestyle Habit") + 
+  ylab("Percentage of Adult Population") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+print(ggplot_object)
+dev.off()
